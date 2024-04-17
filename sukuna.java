5bad0f203e798cb0461e483d5c4a61b4eaa69ba4
igRuban <rubana30062001@gmail.com>
2024-04-17 14:21:08 +0530

    feat:adding shinra Tenshi  print

--- sukuna.java
+++ sukuna.java
@@ -3,5 +3,6 @@ import java.util.*;
 public class Pain{
     public static void main (String[] args){
         System.out.println(" This world shall Know Pain");
+        System.out.println(" Shinra Tensei");
     }
 }
